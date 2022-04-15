1c3a54653252bcb4c762d4e4b29268b815d3b593
Deerdevil <nicholai.oj@gmail.com>
2022-04-15 22:50:36 +0200

    Made some js

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict:";
+
+const bioButton = document.querySelector(".bio--img");
+const aboutMe = document.getElementById("aboutMe");
+const bioClose = document.querySelector(".bio--open");
+const openNav = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav--menu");
+const navClose = document.querySelector(".nav--open");
+//About me
+bioButton.addEventListener("click", function () {
+  aboutMe.classList.add("about--me--mask--clicked");
+  aboutMe.classList.add("about--me--modal--open");
+
+  bioButton.style.display = "none";
+});
+
+bioClose.addEventListener("click", function () {
+  aboutMe.classList.add("hidden");
+  aboutMe.classList.remove("about--me--mask--clicked");
+  aboutMe.classList.remove("about--me--modal--open");
+  setTimeout(() => {
+    aboutMe.classList.remove("hidden");
+  }, 500);
+
+  // aboutMe.classList.remove("about--me--mask");
+  bioButton.style.display = "";
+});
+
+//Open nav
+openNav.addEventListener("click", function () {
+  navMenu.classList.remove("hidden");
+});
+
+navClose.addEventListener("click", function () {
+  navMenu.classList.add("hidden");
+});
